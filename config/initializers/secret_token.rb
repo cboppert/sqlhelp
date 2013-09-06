6950bfecf673e2b2cd918f0e0a93fc6ab8d08b3d
Cody Boppert <cboppert@buffalo.edu>
2013-09-06 01:47:21 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sqlhelp::Application.config.secret_token = '0b92fded25ea2bb1b6c7259b85f3c0127520797f75405e9a58d47328366cc3c80e9674c841381da149dbbd900f5b037e1935cbdae93a33b4058c68946ac6bb3f'
